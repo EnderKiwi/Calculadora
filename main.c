1737a7878dd5a7214afd5e2417d9599d67a8c2b8
EnderKiwi <64765206+EnderKiwi@users.noreply.github.com>
2023-07-10 00:25:46 -0400

    Change spacing on information printing VSCode kept thinking the spacing was 7 spaces...

--- main.c
+++ main.c
@@ -6,23 +6,23 @@
 int main(int argc, char **argv)
 {
      printf("Calculadora v%d.%d.%d-%s build %d\n",
-            CALCULADORA_VERSION_MAJOR,
-            CALCULADORA_VERSION_MINOR,
-            CALCULADORA_VERSION_PATCH,
-            CALCULADORA_VERSION_EXTRA,
-            CALCULADORA_VERSION_BUILD);
+               CALCULADORA_VERSION_MAJOR,
+               CALCULADORA_VERSION_MINOR,
+               CALCULADORA_VERSION_PATCH,
+               CALCULADORA_VERSION_EXTRA,
+               CALCULADORA_VERSION_BUILD);
      printf(": util v%d.%d.%d-%s build %d\n",
-            UTIL_VERSION_MAJOR,
-            UTIL_VERSION_MINOR,
-            UTIL_VERSION_PATCH,
-            UTIL_VERSION_EXTRA,
-            UTIL_VERSION_BUILD);
+               UTIL_VERSION_MAJOR,
+               UTIL_VERSION_MINOR,
+               UTIL_VERSION_PATCH,
+               UTIL_VERSION_EXTRA,
+               UTIL_VERSION_BUILD);
      printf(": math v%d.%d.%d-%s build %d\n",
-            MATH_VERSION_MAJOR,
-            MATH_VERSION_MINOR,
-            MATH_VERSION_PATCH,
-            MATH_VERSION_EXTRA,
-            MATH_VERSION_BUILD);
+               MATH_VERSION_MAJOR,
+               MATH_VERSION_MINOR,
+               MATH_VERSION_PATCH,
+               MATH_VERSION_EXTRA,
+               MATH_VERSION_BUILD);
 
      return 0;
 }
